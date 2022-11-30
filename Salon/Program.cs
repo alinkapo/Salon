df3ed51768853833d1312f3d04e99184e0ac665b
Reims <adimaaskarovna@mail.ru>
2022-11-30 10:03:45 +0400

    Добавьте файлы проекта.

---
+++ Salon/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Salon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Приветствуем вас!");
+
+
+            Console.WriteLine("Выберите команду:");
+
+
+            while (true)
+            {
+                var command = Console.ReadLine().ToLower().Split(' ');
+                Controller s = new Controller();
+                switch (command[0])
+                {
+                    case "добавить":
+                        s.AddGood(command[1], float.Parse(command[2]), float.Parse(command[3]), command[4]);
+                        break;
+                    case "2":
+                        Console.WriteLine(s.GetGoods());
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка в команде");
+                        break;
+
+                }
+            }
+        }
+    }
+}
